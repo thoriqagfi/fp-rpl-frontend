452dea132c8c29261b4d219808efcbf23b2dd344
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2023-04-03 19:28:29 +0700

    fix: refactor landing page into index.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { ImSpinner8 } from 'react-icons/im';
 
 export default function Loading() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center text-gray-800'>
+    <div className='flex min-h-screen flex-col items-center justify-center text-gray-800 bg-white'>
       <ImSpinner8 className='mb-4 animate-spin text-4xl' />
       <p>Loading...</p>
     </div>
